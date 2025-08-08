c9564eb064504d997ae01e7d59a163c507294d52
Vivek G <vivekgilbert123@gmail.com>
2025-08-08 20:28:25 +0530

    new site

---
+++ src/main/java/com/telusko/simplewebapp/homecontroller.java
@@ -0,0 +1,21 @@
+package com.telusko.simplewebapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+public class homecontroller {
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello, welcome to the Simple Web Application!";
+    }
+
+    @RequestMapping("/about")
+    public String ABOUT(){
+        return "THIS IS ABOUT PAGE";
+    }
+    
+}
+        
