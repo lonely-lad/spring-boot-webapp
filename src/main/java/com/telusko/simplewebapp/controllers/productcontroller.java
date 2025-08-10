b5a58468b1a0ce7cf691f0d970765815e3f360f9
Vivek G <vivekgilbert123@gmail.com>
2025-08-10 12:34:20 +0530

    product

--- src/main/java/com/telusko/simplewebapp/controllers/productcontroller.java
+++ src/main/java/com/telusko/simplewebapp/controllers/productcontroller.java
@@ -1,16 +1,25 @@
 package com.telusko.simplewebapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.telusko.simplewebapp.models.product;
+import com.telusko.simplewebapp.servises.productservices;
+@RestController
 public class productcontroller {
+
     // This class will handle product-related functionality
     // Currently, it is empty and can be implemented later
     // For example, you can add methods to handle product requests
     // and return appropriate responses based on product data.
-    
+    @Autowired
+    productservices service;
     // Placeholder method for product listing
     @RequestMapping("/products")
-    public String getProducts() {
-        return "";
+    public List<product> getProducts() {
+        return service.getAllProducts();
     }
 }
--- src/main/java/com/telusko/simplewebapp/servises/productservices.java
+++ src/main/java/com/telusko/simplewebapp/servises/productservices.java
@@ -3,13 +3,18 @@ package com.telusko.simplewebapp.servises;
 import java.util.Arrays;
 import java.util.List;
 
-import com.telusko.simplewebapp.models.product;
+import org.springframework.stereotype.Service;
 
+import com.telusko.simplewebapp.models.product;
+@Service
 public class productservices {
 
-    List<product> products=Arrays.asList(new product(101,"iphone",50000));
+    List<product> products=Arrays.asList(new product(101,"iphone",50000)
+            ,new product(102,"samsung",30000)
+            ,new product(103,"oneplus",25000)
+            ,new product(104,"nokia",15000));
     public List<product> getAllProducts() {
         // Implementation to retrieve all products
-        return null; // Placeholder return
+        return products; // Placeholder return
     }
 }
